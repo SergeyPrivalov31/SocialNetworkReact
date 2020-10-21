07fa25b2edad5a733d828f23b3d579c0bd86b778
Sergey <68424586+SergeyPrivalov31@users.noreply.github.com>
2020-10-21 10:19:36 +0300

    upload files
    comlete install

---
+++ src/maps.js
@@ -0,0 +1,11 @@
+import openGoogle from './google.js';
+import openYandex from './yandex.js';
+
+let openAllMaps = () => {
+
+    openGoogle();
+    openYandex();
+
+}
+
+export default openAllMaps;
---
+++ src/yandex.js
@@ -0,0 +1,6 @@
+let openYandex = () => {
+    // connect to Yandex
+    // show map //modules AMD require.js
+}
+
+export default openYandex;
