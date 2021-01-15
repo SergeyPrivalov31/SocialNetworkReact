3143b97715d20a8f69d242f4f3edd73746cd423f
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-01-15 13:01:12 +0300

    Pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,20 +4,40 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user-photo.png'
 
 export class Users extends React.Component {
-
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount / 100); // разделил временно на 100, нужно доработать pagination
             });
     }
 
+    onPageChanged = (pageNumber) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+        this.props.setCurrentPage(pageNumber);
+    }
+
     render() {
-        return (
-            <div>
-                {/*<button onClick={this.getUsers}>Get Users</button>*/}
-                {
-                    this.props.users.map(u => <div key={u.id}>
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
+        return (<div>
+                <div className={styles.cursor}>
+                    {pages.map(p => {
+                        return <span className={this.props.currentPage === p && styles.selectedPage}
+                        onClick={ (e) => {this.onPageChanged(p);}}>{p}</span>
+                    })}
+                </div>
+                {this.props.users.map(u => <div key={u.id}>
             <span>
             <div>
             <img src={u.photos.small != null ? u.photos.small : userPhoto}
@@ -33,7 +53,7 @@ export class Users extends React.Component {
                 }}>Follow</button>}
             </div>
             </span>
-                        <span>
+                    <span>
             <span>
             <div>{u.name}</div>
             <div>{u.status}</div>
@@ -44,9 +64,10 @@ export class Users extends React.Component {
             <div>{"u.location.city"}</div>
             </span>
             </span>
-                    </div>)
+                </div>)
                 }
-            </div>)
+            </div>
+        )
 
     }
 }
