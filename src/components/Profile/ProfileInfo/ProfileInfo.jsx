fa1799dddb6c19c07c2c5cb883cd19a6af76d331
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-01-16 08:47:04 +0300

    Create UsersPage

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 
+
 const ProfileInfo = (props) => {
 
-    if(!props.profile) {
-        return <Preloader />
+    if (!props.profile) {
+        return <Preloader/>
     }
 
     return (
-        <div>
-            <div>
-                <img className={s.wallpepper}
-                     src="https://img3.akspic.ru/previews/1/3/6/8/98631/98631-plyazh-pribrezhnyye_i_okeanicheskiye_relyefy-karibskij_bassejn-tropicheskaya_zona-komnata-x750.jpg"/>
-                <div className={s.description}>
-                    <div className={s.ava}>
-                        <img src={props.profile.photos.large}/>
-                    </div>
-                </div>
+        <div className={s.ava_description}>
+            <div className={s.ava}>
+                <img src={props.profile.photos.large}/>
             </div>
-
+            <div className={s.discription}>
+                Description HERE
+            </div>
         </div>
-    );
+    )
 }
 export default ProfileInfo;
