86f22f7ab56e73ab7ab66eb3024a5253da4fd67f
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-02-01 13:44:05 +0300

    Reselect

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {createSelector} from "reselect";
 
-export const getUsers = (state) => {
+
+
+const getUsersSelector = (state) => {
     return state.usersPage.users
 }
+export const getUsers = createSelector(getUsersSelector,(users) => {
+    return users.filter(u => true)
+})
+
+
+
 export const getPageSize = (state) => {
     return state.usersPage.pageSize
 }
