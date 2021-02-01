7e4bf06c19ffca93edb77f90f57184ede25c1780
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-02-01 15:44:38 +0300

    React HOOKs. UseState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
-import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
 
@@ -13,7 +13,7 @@ const ProfileInfo = (props) => {
         <div className={s.ava_description}>
             <div className={s.ava}>
                 <img src={props.profile.photos.small} alt={' '}/>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
             </div>
             <div className={s.description}>
                 <ul>{props.profile.fullName}</ul>
